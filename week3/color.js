bf57bcdd9e232a091a478e030a357f38696b20bc
imagest108 <imagest108@gmail.com>
2013-02-15 14:46:33 -0500

    week3 in-class codes

---
+++ week3/color.js
@@ -0,0 +1,7 @@
+// create an array of colors
+var colors = ['Red','Blue','Green','Magenta'];
+
+// loop and print each array number + color
+for (i=0; i < colors.length; i++) {
+    console.log(i + " " + colors[i]);
+}
